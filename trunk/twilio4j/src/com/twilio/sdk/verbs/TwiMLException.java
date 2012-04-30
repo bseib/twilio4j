8e0d53d477c949584aa651d4b0ef755065e5db77
broc.seib@gmail.com <broc.seib@gmail.com@46044b12-74ca-2fc0-0e39-95be6b061b52>
2012-04-30 18:57:59 +0000

    copied in an older version of the twilio java code

---
+++ trunk/twilio4j/src/com/twilio/sdk/verbs/TwiMLException.java
@@ -0,0 +1,10 @@
+package com.twilio.sdk.verbs;
+
+public class TwiMLException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public TwiMLException(String arg0) {
+        super(arg0);
+    }
+
+}
