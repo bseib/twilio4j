54302ba02c36d16db18ce06c0882353e37d5f2f3
broc.seib@gmail.com <broc.seib@gmail.com@46044b12-74ca-2fc0-0e39-95be6b061b52>
2012-04-30 18:57:59 +0000

    copied in an older version of the twilio java code

---
+++ twilio4j/src/com/twilio/sdk/TwilioRestException.java
@@ -0,0 +1,10 @@
+package com.twilio.sdk;
+
+public class TwilioRestException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public TwilioRestException(String arg0) {
+        super(arg0);
+    }
+
+}
