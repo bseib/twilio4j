92800832bf9c53371620b416304784f322796975
broc.seib@gmail.com <broc.seib@gmail.com@46044b12-74ca-2fc0-0e39-95be6b061b52>
2012-09-07 03:19:06 +0000

    Implemented an example of a Queue that does the same demonstration as the Twilio screencast of the same topic.

--- twilio4j/src/com/twilio4j/twism/eg/CallCenterState.java
+++ twilio4j/src/com/twilio4j/twism/eg/CallCenterState.java
@@ -17,13 +17,8 @@ package com.twilio4j.twism.eg;
 
 public enum CallCenterState {
 	// by convention handlers start with a H_
-	H_GATHER_CALL_IN_CODE,
-	H_CHECK_CALL_IN_CODE,
-	H_RECORD_MESSAGE,
-	H_REVIEW_MESSAGE,
-	H_REVIEW_MESSAGE_CHOICE,
-	H_MESSAGE_READY_GOODBYE,
+	H_CALL_CENTER_HOTLINE,
+	H_HOLD_MUSIC,
+	H_CALL_CENTER_AGENT,
 
-	// by convention callbacks start with a C_
-	C_HANGUP,
 }
